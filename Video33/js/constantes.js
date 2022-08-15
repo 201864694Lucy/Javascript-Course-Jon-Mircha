733041acf80edc45e4cf76192346ec7fb6b444da
Lucy Palacios <48799273+201864694Lucy@users.noreply.github.com>
2022-08-15 00:40:50 -0500

    Add files via upload

---
+++ Video33/js/constantes.js
@@ -0,0 +1,14 @@
+export const PI = Math.PI;
+export let usuario = "Lucy";
+
+const password = "Morado12";
+const hola = () => console.log("Hola");
+
+export default function saludar() {
+  console.log("Hola Módulos +ES6");
+}
+export class Saludar {
+  constructor() {
+    console.log("Hola a todos");
+  }
+}
---
+++ Video33/js/modulos.js
@@ -0,0 +1,10 @@
+import saludar, { Saludar, PI, usuario } from "./constantes.js";
+import { aritmetica as calculos } from "./aritmetica.js";
+
+console.log("Archivo modulos.js");
+console.log(PI, usuario);
+console.log(calculos.sumar(2, 6));
+console.log(calculos.restar(2, 6));
+saludar();
+let saludo = new Saludar();
+saludo;
